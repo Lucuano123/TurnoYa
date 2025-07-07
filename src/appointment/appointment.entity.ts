311eb263fecd907449528c0b4aeaf54d8906d213
Jonatan Harbertz <129443750+JHProyectos@users.noreply.github.com>
2025-07-07 09:20:35 -0300

    Update appointment.entity.ts

--- src/appointment/appointment.entity.ts
+++ src/appointment/appointment.entity.ts
@@ -3,8 +3,8 @@ export class Appointment {
     constructor(
         public date: Date,
         public time: string,
-        public custumer_name: string,
-        public custumer_email: string,
+        public customer_name: string,
+        public customer_email: string,
         public booked_service: string,
         public status: 'scheduled' | 'completed' | 'canceled'
     ) {}
