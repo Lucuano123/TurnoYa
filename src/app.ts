875014012eab0ca064c8d5af7a55fad9bfe0458a
Jonatan Harbertz <jhproyectosvt@gmail.com>
2025-07-21 17:04:55 -0300

    cambio de nombre de carpeta customer

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { customerRouter } from './custumer/customer.routes.js';
+import { customerRouter } from './customer/customer.routes.js';
 import { appointmentRouter } from './appointment/appointment.routes.js';
 
 const app = express();
