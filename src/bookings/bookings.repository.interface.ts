5c445b205175d3308d6080b85a59e39775fc4938
Lucuano123 <luliquaranta6@gmail.com>
2025-11-14 14:47:01 -0300

    corrección rutas booking

--- src/bookings/bookings.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-/*import { Booking } from './bookings.entity.js';
-
-export interface BookingsRepository {
-
-  add(booking: Booking): Promise<Booking>;
-  getProfessionalBookings(date: string): Promise<Booking[]>;
-
-}*/
--- src/bookings/bookings.routes.ts
+++ src/bookings/bookings.routes.ts
@@ -11,12 +11,11 @@ bookingsRouter.get(
 );
 
 // Definición de rutas
-
-bookingsRouter.post('/', sanitizeBookingInput, bookingsController.addBookings.bind(bookingsController));
-bookingsRouter.get('/api/bookings', sanitizeBookingInput, bookingsController.getAllBookings.bind(bookingsController))
-bookingsRouter.get('/api/bookings/:id', sanitizeBookingInput, bookingsController.getBookingById.bind(bookingsController))
-//bookingsRouter.put('/api/bookings/:id', sanitizeBookingInput, bookingsController.updateBooking.bind(bookingsController))
-//bookingsRouter.delete('/api/bookings/:id', sanitizeBookingInput, bookingsController.deleteBooking.bind(bookingsController))
+bookingsRouter.post('/', bookingsController.addBookings.bind(bookingsController));
+bookingsRouter.get('/', bookingsController.getAllBookings.bind(bookingsController));
+bookingsRouter.get('/:id', bookingsController.getBookingById.bind(bookingsController));
+//bookingsRouter.put('/api/bookings/:id', bookingsController.updateBooking.bind(bookingsController))
+//bookingsRouter.delete('/api/bookings/:id', bookingsController.deleteBooking.bind(bookingsController))
 
 
 function sanitizeBookingInput(req:any, res:any, next:any) {
